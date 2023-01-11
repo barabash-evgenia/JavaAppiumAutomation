0a03b954c2311a2c1a332f940b3e0583aec20852
evgenia_barabash <barabash.evgeniya@gmail.com>
2023-01-11 19:16:56 +0300

    Lesson 1 task 2

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,12 +1,27 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MainClassTest {
+
     MainClass mainClass = new MainClass();
+
     @Test
     public void testGetLocalNumber(){
         int a = mainClass.getLocalNumber();
         if (a != 14) {
-            System.out.println("Возвращаемое методом getLocalNumber() значение '" + a + "' не совпало с ожидаемым '14'.");
+            Assert.fail("Возвращаемое методом getLocalNumber() значение " + a + " не совпало с ожидаемым 14.");
+        }
+    }
+
+    @Test
+    public void testGetClassNumber(){
+        int a = mainClass.getClassNumber();
+        if (a <= 45) {
+            if (a == 45) {
+                Assert.fail("Возвращаемое методом getClassNumber() значение равно 45.");
+            } else {
+                Assert.fail("Возвращаемое методом getClassNumber() значение " + a + " меньше 45.");
+            }
         }
     }
 }
